cf9bbf669e56ca95d6a5462f0fce46be3f90963a
first-branch <radeva7609@gmaill.com>
2025-11-02 12:43:33 +0200

    Initial commit of Auth0 integration for backend project

--- src/main/java/com/example/helloworld/config/security/SecurityConfig.java
+++ src/main/java/com/example/helloworld/config/security/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.example.helloworld.config.security;
 
+import com.example.helloworld.config.CustomJwtAuthConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,15 +20,26 @@ public class SecurityConfig {
     private final AuthenticationErrorHandler authenticationErrorHandler;
 
     @Bean
-    public SecurityFilterChain httpSecurity(final HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/api/messages/protected", "/api/messages/admin").authenticated()
-                        .anyRequest().permitAll())
-                .cors(Customizer.withDefaults())
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())
-                        .authenticationEntryPoint(authenticationErrorHandler))
-                .build();
+    public JwtAuthenticationConverter customJwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new CustomJwtAuthConverter());
+        return converter;
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/messages/public").permitAll()
+                        .requestMatchers("/api/messages/protected").authenticated()
+                        .requestMatchers("/api/messages/admin").hasRole("Admin")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(customJwtAuthenticationConverter()))
+                );
+
+        return http.build();
     }
 }
--- src/main/java/com/example/helloworld/controllers/MessageController.java
+++ src/main/java/com/example/helloworld/controllers/MessageController.java
@@ -1,14 +1,19 @@
 package com.example.helloworld.controllers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 import com.example.helloworld.models.Message;
 import com.example.helloworld.services.MessageService;
 
 import lombok.RequiredArgsConstructor;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/messages")
@@ -26,8 +31,15 @@ public class MessageController {
         return messageService.getProtectedMessage();
     }
 
+    @PreAuthorize("hasRole('Admin')")
     @GetMapping("/admin")
-    public Message getAdmin() {
+    public Message getAdmin(@AuthenticationPrincipal Jwt jwt) {
+        log.info("User sub: {}", jwt.getSubject());
+        log.info("Email: {}", jwt.getClaimAsString("email"));
+        log.info("Name: {}", jwt.getClaimAsString("name"));
+
+
         return messageService.getAdminMessage();
     }
+
 }
